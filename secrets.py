d39d73c4215311805fec9bbb9e1e59171826c20d
Äkäslompolo Simppa <sjjamsa@simpson-25.org.aalto.fi>
2026-05-26 07:24:54 +0300

    Check for secrets

--- secrets.py
+++ secrets.py
@@ -1,7 +1,21 @@
 import os
 
-URL = os.environ["URL"]
-INFLUX_URL = os.environ["INFLUX_URL"]
-INFLUX_TOKEN = os.environ["INFLUX_TOKEN"]
-INFLUX_ORG = os.environ["INFLUX_ORG"]
+required = [
+    "URL",
+    "INFLUX_URL",
+    "INFLUX_TOKEN",
+    "INFLUX_ORG",
+    "INFLUX_BUCKET"
+]
+
+for key in required:
+    value = os.environ.get(key)
+
+    if not value:
+        raise RuntimeError(f"Missing env variable: {key}")
+
+URL           = os.environ["URL"]
+INFLUX_URL    = os.environ["INFLUX_URL"]
+INFLUX_TOKEN  = os.environ["INFLUX_TOKEN"]
+INFLUX_ORG    = os.environ["INFLUX_ORG"]
 INFLUX_BUCKET = os.environ["INFLUX_BUCKET"]
