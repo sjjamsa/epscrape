4aaa8d27b794d646f70a570d5825ff2a1ba2496e
Äkäslompolo Simppa <sjjamsa@simpson-25.org.aalto.fi>
2026-05-26 07:27:25 +0300

    goto timeout

--- get_json.py
+++ get_json.py
@@ -94,7 +94,7 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
-    page.goto(URL, wait_until="networkidle")
+    page.goto(URL, wait_until="networkidle", timeout=3000)
     page.wait_for_timeout(3000)
 
     blocks = {
